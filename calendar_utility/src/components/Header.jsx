84b9c16d02f958f3722e0ff6839283bd63774d83
unmemero <fafagm99@gmail.com>
2025-08-29 23:24:54 -0600

    Added GH checks rules

---
+++ calendar_utility/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Calendar() {
+    return (
+        <header>
+            <h1>Timesheet information</h1>
+            <nav>
+                <Link></Link>
+            </nav>
+        </header>
+    );
+}
