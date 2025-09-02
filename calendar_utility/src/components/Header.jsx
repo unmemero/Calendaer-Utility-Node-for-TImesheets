b502f47cb1fa39f78d2cfbd89d1b36340ba96d6e
unmemero <fafagm99@gmail.com>
2025-09-01 23:45:34 -0600

    .

--- calendar_utility/src/components/Header.jsx
+++ calendar_utility/src/components/Header.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-let api_key = "my_secret_key";
-
 export default function Calendar() {
     return (
         <header>
             <h1>Timesheet information</h1>
-            <p>API Key: {api_key}</p>
             <nav>
                 <Link></Link>
             </nav>
